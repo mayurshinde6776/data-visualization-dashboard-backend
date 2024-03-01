43117b16d801d4830c6b8cadcfabfd6560fc9cec
Mayur-Thinkbridge <mayurshinde6776@gmail.com>
2024-03-01 17:37:31 +0530

    initial app setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
